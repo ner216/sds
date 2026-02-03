207a948d13f2b49a7f7131fc56059cc045fca88d
ner216 <provencher.nolan@gmail.com>
2026-02-03 10:01:39 -0500

    feat: Add icon for start window as well as requirements.txt file.

--- src/ui/main_window.py
+++ src/ui/main_window.py
@@ -1,4 +1,5 @@
 from PyQt6.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QLineEdit, QPushButton, QLabel
+from PyQt6.QtGui import QPixmap
 from PyQt6.QtCore import Qt
 
 class MainWindow(QMainWindow):
@@ -13,6 +14,13 @@ class MainWindow(QMainWindow):
         layout = QVBoxLayout(central_widget)
         layout.setSpacing(15)
         layout.setContentsMargins(30, 30, 30, 30)
+
+        self.logo_label = QLabel()
+        pixmap = QPixmap("assets/main-icon-blue.png")
+        scaled_pixmap = pixmap.scaled(120, 120, Qt.AspectRatioMode.KeepAspectRatio, Qt.TransformationMode.SmoothTransformation)
+        self.logo_label.setPixmap(scaled_pixmap)
+        self.logo_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        layout.addWidget(self.logo_label)
 
         # UI Elements
         self.label = QLabel("Enter Master Password")

45ba1dbe5d99d341cafe5d4f2ead975c58081e55
ner216 <provencher.nolan@gmail.com>
2026-04-10 12:05:46 -0400

    feat(cli): Add methods for interactive mode and deleting/adding passwords to the CLI.

--- src/main.py
+++ src/main.py
@@ -12,9 +12,12 @@ def main():
     default_safe_path = SafeConfig().get_default_safe_path()
 
     parser = argparse.ArgumentParser(description="A simple password manager.")
-    parser.add_argument("-d", "--delete-config", action="store_true", help="Delete all config data created by SDS")
-    parser.add_argument("-g", "--get-password", type=str, help="Get a password from the default safe.")
+    parser.add_argument("-a", "--add-password", action="store_true", help="Add a new password entry to the default safe.")
+    parser.add_argument("-d", "--delete-password", type=str, help="Delete a password from the default safe.")
+    parser.add_argument("-g", "--get-password", type=str, help="Get password from default safe given the program-name/id.")
     parser.add_argument("-l", "--list", action="store_true", help="List saved password entries.")
+    parser.add_argument("-i", "--interactive", action="store_true", help="Interactive mode to perform many actions in one login session.")
+    parser.add_argument("--delete-config", action="store_true", help="Delete all config data created by SDS.")
 
     args = parser.parse_args()
 
@@ -23,8 +26,14 @@ def main():
         start_gui()
     elif args.list == True:
         CLI(default_safe_path).list_entries()
+    elif args.add_password == True:
+        CLI(default_safe_path).add_password()
     elif args.delete_config == True:
         SafeConfig().delete_all()
+    elif args.interactive == True:
+        CLI(default_safe_path).interactive_mode()
+    elif args.delete_password:
+        CLI(default_safe_path).delete_password()
     elif args.get_password:
         CLI(default_safe_path).get_password(args.get_password)
 
--- src/ui/cli.py
+++ src/ui/cli.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import getpass
 
 from core.hash_logic import Hash
@@ -5,14 +6,51 @@ from core.database import PasswordDB
 
 class CLI():
     def __init__(self, db_file_path: str):
-        passphrase = getpass.getpass("Password (input hidden) > ")
         self.db_file_path = db_file_path
+        self.db_name = Path(db_file_path).stem
+        passphrase = getpass.getpass(f"Password for {self.db_name} (input hidden) > ")
         
         try:
             self.db = PasswordDB(self.db_file_path, passphrase)
             self.db.load_db()
         except Exception as e:
             print("[ERROR] Unable to open database. Wrong password?")
+
+    def add_password(self) -> None:
+        if self.db.password_locked: return
+
+        print("Add password details:")
+        
+        app = input("Program > ")
+        user = input("Username > ")
+        passwd = input("Password > ")
+
+        try:
+            self.db.add_entry(app, user, passwd)
+            print(f"[INFO] Added password for {site}!")
+        except Exception as e:
+            print(f"[ERROR] Unable to save new password for {site}!\n Err: {e}")
+
+    def delete_password(self, username_or_id: str):
+        if self.db.password_locked: return
+
+        entry_to_delete = None
+
+        for entry in self.db.get_entries():
+            if not username.isdigit():
+                if entry["site"].lower() == username.lower():
+                    entry_to_delete = entry
+                    break
+            else:
+                if str(entry["id"]) == username:
+                    entry_to_delete = entry
+                    break
+
+        if entry_to_delete is not None:
+            self.db.delete_entry(entry["id"])
+            print(f"[INFO] Deleted Password for {entry["site"]}")
+        else:
+            print(f"[INFO] Database does not contain an entry for {username_or_id}")
 
     def get_password(self, username: str):
         if self.db.password_locked: return
@@ -24,10 +62,12 @@ class CLI():
                 if entry["site"].lower() == username.lower():
                     print(f"Password for {entry["site"]} > '{entry["pass"]}'")
                     entry_found = True
+                    break
             else:
                 if str(entry["id"]) == username:
                     print(f"Password for {entry["site"]} > '{entry["pass"]}'")
                     entry_found = True
+                    break
 
         if entry_found == False:
             print(f"[INFO] Database does not contain an entry for {username}")
@@ -38,4 +78,41 @@ class CLI():
         print("Saved Password Entries:")
         for entry in self.db.get_entries():
             print(f"  {entry["id"]}: {entry["site"]}")
+
+    def interactive_mode(self):
+        if self.db.password_locked: return
+        choice = ""
+
+        def print_operations():
+            print(f"Operations for {self.db_name}:")
+            print(" 1: (add) add a password")
+            print(" 2: (del) delete a password")
+            print(" 3: (get) get a password")
+            print(" 4: (ls) list a passwords")
+            print(" h: (help) print this list")
+            print(" q: (quit)")
+
+        print_operations()
+
+        while choice != "q" or choice != "quit":
+            choice = input("Operation > ").lower()
+
+            if choice == "1" or choice == "add":
+                self.add_password()
+            elif choice == "2" or choice == "del":
+                name_or_id = input("Enter ID or name of entry to delete > ")
+                self.delete_password(name_or_id)
+            elif choice == "3" or choice == "get":
+                name_or_id = input("Enter ID or name of entry to get > ")
+                self.get_password(name_or_id)
+            elif choice == "4" or choice == "ls":
+                self.list_entries()
+            elif choice == "h" or choice == "help":
+                print_operations()
+            elif choice == "q" or choice == "quit":
+                break
+        
+
+
+
         

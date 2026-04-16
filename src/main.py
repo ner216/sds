6631792aeacd3d793495a5ba0371449abe03de01
ner216 <provencher.nolan@gmail.com>
2026-04-15 23:00:16 -0400

    feat: Add CLI option to specify safe path.

--- src/main.py
+++ src/main.py
@@ -16,28 +16,35 @@ def main():
     parser.add_argument("-g", "--get-password", type=str, help="Get password from default safe given the program-name/id.")
     parser.add_argument("-l", "--list", action="store_true", help="List saved password entries.")
     parser.add_argument("-i", "--interactive", action="store_true", help="Interactive mode to perform many actions in one login session.")
+    parser.add_argument("-p", "--path", type=str, help="Use safe at specified PATH.")
     parser.add_argument("--delete-config", action="store_true", help="Delete all config data created by SDS.")
     parser.add_argument("--backup-safe", action="store_true", help="Backup the default safe as plain text.")
 
     args = parser.parse_args()
 
+    # Determine if specific path was used
+    if args.path:
+        safe_path = args.path
+    else:
+        safe_path = default_safe_path
+
     if len(sys.argv) == 1:
         # Start GUI if no args are given
         start_gui()
     elif args.list == True:
-        CLI(default_safe_path).list_entries()
+        CLI(safe_path).list_entries()
     elif args.add_password == True:
-        CLI(default_safe_path).add_password()
+        CLI(safe_path).add_password()
     elif args.delete_config == True:
         SafeConfig().delete_all()
     elif args.backup_safe == True:
-        CLI(default_safe_path).backup_entries()
+        CLI(safe_path).backup_entries()
     elif args.interactive == True:
-        CLI(default_safe_path).interactive_mode()
+        CLI(safe_path).interactive_mode()
     elif args.delete_password:
-        CLI(default_safe_path).delete_password(args.delete_password)
+        CLI(safe_path).delete_password(args.delete_password)
     elif args.get_password:
-        CLI(default_safe_path).get_password(args.get_password)
+        CLI(safe_path).get_password(args.get_password)
 
 def start_gui():
     app = QApplication(sys.argv)
